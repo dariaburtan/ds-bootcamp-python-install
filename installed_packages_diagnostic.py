0957a3798ddabf4457d7b4ba96e2dd0af39aa4e7
Tom Fowler <tom.fowler@multiverse.io>
2021-12-16 08:54:25 +0000

    Updated diagnostic script

--- installed_packages_diagnostic.py
+++ installed_packages_diagnostic.py
@@ -1,7 +1,7 @@
 # Script for testing whether packages/modules are installed.
 
 # Author: Tom Fowler
-# Version: 0.0.0.9000
+# Version: 0.0.0.9001
 # Last Update: 2021-12-15
 
 # Imports
@@ -11,19 +11,20 @@ import importlib.util as imputil
 # List of packages to test presence for
 packages = ["math", "numpy", "pandas", "matplotlib", "seaborn", "scipy",
             "sklearn", "statsmodels", "nltk", "TextBlob", "bs4",
-            "requests", "datetime", "random"]
+            "requests", "datetime", "random", "jupyter", "jupyterlab",
+            "IPython"]
 
 # Define function
 def test_installed(pkg):
-    
+
     if pkg in sys.modules:
-        print(f'Python module \'{pkg}\' is already installed in sys.modules\n')
+        print(f'\'{pkg}\' found in sys.modules\n')
         out = True
     elif imputil.find_spec(pkg) != None:
-        print(f'A library matching the name \'{pkg}\' is available for import\n')
+        print(f'\'{pkg}\' found with find_spec()\n')
         out = True
     else:
-        print(f'Could not find {pkg} in sys.modules or with implib.find_spec()\n')
+        print(f'**Missing pkg**: \'{pkg}\' not in sys.modules or found with implib.find_spec()\n')
         out = False
     return out
 
